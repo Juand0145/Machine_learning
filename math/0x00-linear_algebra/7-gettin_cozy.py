e971339b5659db6dc8fe97ff0f14b16b388d07a3
Juand0145 <juand0145@gmail.com>
2021-11-30 17:27:09 -0500

    [Adding] Task 7, 8 and 9

---
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Is a funtion that concatenates two matrices along a specific axis"""
+import numpy as np
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """funtion that concatenates two matrices along a specific axis"""
+    matrix_1 = np.matrix(mat1)
+    matrix_2 = np.matrix(mat2)
+
+    matrix_3 = np.concatenate((matrix_1, matrix_2), axis).tolist()
+
+    return matrix_3
---
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Is a function that performs matrix multiplication"""
+import numpy as np
+
+
+def mat_mul(mat1, mat2):
+    """Function that performs matrix multiplication"""
+
+    columns = len(mat1[0])
+    rows = len(mat2)
+
+    if columns != rows:
+        return None
+
+    mat1 = np.matrix(mat1)
+    mat2 = np.matrix(mat2)
+
+    mat3 = np.matmul(mat1, mat2).tolist()
+
+    return mat3

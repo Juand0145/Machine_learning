5c1447f5fc6365010d1e4a124c4355199ff28c70
Juand0145 <juand0145@gmail.com>
2021-12-01 09:24:02 -0500

    [Fixing] chaging the try statement

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -4,10 +4,9 @@
 
 def matrix_shape(matrix):
     """Function that calculates the shape of a matrix"""
-    try:
-        iter(matrix)
-        elements = len(matrix)
+    elements = len(matrix)
+
+    if type(matrix[0]) is list:
         return [elements] + matrix_shape(matrix[0])
 
-    except:
-        return [elements]
+    return [elements]

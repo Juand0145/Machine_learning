47f5d2fdd7fb40d34ca6c4194b1bdfe2980015db
Juand0145 <juand0145@gmail.com>
2021-12-01 09:15:32 -0500

    [Fixing] changing try by if statement

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -5,13 +5,17 @@
 def cat_matrices2D(mat1, mat2, axis=0):
     """funtion that concatenates two matrices along a specific axis"""
     import numpy as np
-    try:
-        matrix_1 = np.matrix(mat1)
-        matrix_2 = np.matrix(mat2)
 
-        matrix_3 = np.concatenate((matrix_1, matrix_2), axis).tolist()
+    if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+    if axis == 1:
+        if len(mat1) != len(mat2):
+            return None
 
-        return matrix_3
+    matrix_1 = np.matrix(mat1)
+    matrix_2 = np.matrix(mat2)
 
-    except:
-        return None
+    matrix_3 = np.concatenate((matrix_1, matrix_2), axis).tolist()
+
+    return matrix_3

6ab082bb68608b9cbc2ae601c7508df5a9a9a99a
Juand0145 <juand0145@gmail.com>
2021-12-01 10:11:51 -0500

    [Fixing] moving the numpy library to the header

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Is a function that concatenates two matrices along a specific axis"""
+import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
     """Function that concatenates two matrices along a specific axis"""
-    import numpy as np
 
     matrix_3 = np.concatenate((mat1, mat2), axis)
 
--- math/0x00-linear_algebra/14-saddle_up.py
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Is a function  that performs matrix multiplication"""
+import numpy as np
 
 
 def np_matmul(mat1, mat2):
     """Function  that performs matrix multiplication"""
-    import numpy as np
 
     mat3 = np.matmul(mat1, mat2)
 

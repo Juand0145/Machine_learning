30c5596f9ed4ae59d3da8fb894ae882c4ff55748
Juan David Tuta Botero <77861240+Juand0145@users.noreply.github.com>
2021-12-06 12:08:50 -0500

    [Adding] Task 4

---
+++ math/0x01-plotting/4-frequency.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+plt.hist(student_grades, bins=10, edgecolor="black")
+plt.xlabel("Grades")
+plt.ylabel("Number of StudentsNumber of Students")
+plt.show()

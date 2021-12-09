e4652231398ae94998a76a69c0cace32cedb8811
Juand0145 <juand0145@gmail.com>
2021-12-09 12:18:50 -0500

    [Fixing] adding somo changes

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -4,25 +4,21 @@
 
 def poly_integral(poly, C=0):
     """Function that calculates the integral of a polynomial"""
-    # if type(poly) is not list or len(poly) < 1:
-    #     return None
-
-    # integrated_coefficient = [C]
-
-    # for power, coefficient in enumerate(poly):
-    #     if power != 0:
-    #         new_coefficient = coefficient / (power + 1)
-    #         integrated_coefficient.append(new_coefficient)
-
-    #     else:
-    #         new_coefficient = coefficient
-    #         integrated_coefficient.append(new_coefficient)
-
-    # return integrated_coefficient
-
-    if not all(type(C) in (float, int) for c in poly) or type(C) != int:
+    if type(poly) is not list or type(C) not in (int, float):
         return None
-    integral = [c/i if c % i != 0 else c//i for i, c in enumerate(poly, 1)]
-    while len(integral) > 0 and integral[-1] == 0:
-        integral.pop()
-    return [C] + integral
+
+    integrated_coefficient = [C]
+
+    for power, coefficient in enumerate(poly):
+        if coefficient == 0:
+            integrated_coefficient.append(0)
+
+        elif power != 0:
+            new_coefficient = coefficient / (power + 1)
+            integrated_coefficient.append(new_coefficient)
+
+        else:
+            new_coefficient = coefficient
+            integrated_coefficient.append(new_coefficient)
+
+    return integrated_coefficient

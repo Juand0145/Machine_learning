ea3645a4b41b82e4105194aa9863c7d30afa09fe
Juand0145 <juand0145@gmail.com>
2021-12-14 14:28:51 -0500

    [Adding] task 3

---
+++ math/0x03-probability/exponential.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""File that contain the class that represents an
+exponential distribution"""
+
+
+class Exponential:
+    """Class that represents an exponential distribution"""
+    e = 2.7182818285
+    pi = 3.1415926536
+
+    def __init__(self, data=None, lambtha=1.):
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = len(data) / sum(data)

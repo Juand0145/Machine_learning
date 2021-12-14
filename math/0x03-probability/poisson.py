c0dfd8e5476e1372e67badb70607b3e447d4ab6b
Juand0145 <juand0145@gmail.com>
2021-12-14 12:42:56 -0500

    [Fiixng] adding the constant

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -4,12 +4,15 @@
 
 class Poisson:
     """Class that represents a poisson distribution"""
+    e = 2.7182818285
+    pi = 3.1415926536
 
     def __init__(self, data=None, lambtha=1.):
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = lambtha
+
         else:
             if type(data) is not list:
                 raise TypeError("data must be a list")

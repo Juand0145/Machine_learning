a596936b2298c6fc02ff0f9a983ef78db69a296b
Juand0145 <juand0145@gmail.com>
2021-12-14 12:38:12 -0500

    [fixing] finxing task 0

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -2,34 +2,16 @@
 """Its a file tha cotain a class that represents a poisson distribution"""
 
 
-class Poisson():
-    """Class that represents a poisson distribution"""
-    e = 2.7182818285
-    pi = 3.1415926536
-
+class Poisson:
+    """ Poisson distribution class """
     def __init__(self, data=None, lambtha=1.):
-        """Calculating the lmabtha=mean of the poisson distribution"""
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = lambtha
-
         else:
             if type(data) is not list:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            # self.lambtha = np.mean(data)
-            self.lambtha = sum(data)/len(data)
-
-    # def pmf(self, k):
-    #     import math
-    #     """Method the value of the PMF Probability Mass Functions
-    #     for a given number of successes"""
-    #     if type(k) is not int:
-    #         self.k = int(k)
-    #     if k < 0:
-    #         return 0
-
-    #     PMF = ((self.e**-self.lambtha)*(self.lambtha ** k))/math.factorial(k)
-    #     return PMF
+            self.lambtha = sum(data) / len(data)

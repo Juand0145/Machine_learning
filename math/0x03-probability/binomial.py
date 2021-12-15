1f5bd098cdfd7090adf0c5830e7b3ef98f5779da
Juand0145 <juand0145@gmail.com>
2021-12-15 17:38:50 -0500

    [Fixing] task 10

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -9,7 +9,6 @@ class Binomial:
     pi = 3.1415926536
 
     def __init__(self, data=None, n=1, p=0.5):
-        """ initialize constructor for binomial class """
         if data is None:
             if n <= 0:
                 raise ValueError("n must be a positive value")
@@ -17,14 +16,16 @@ class Binomial:
                 raise ValueError("p must be greater than 0 and less than 1")
             self.n = n
             self.p = p
+
         else:
             if type(data) is not list:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
+
             mean = sum(data) / len(data)
             variance = sum([(x - mean) ** 2 for x in data]) / len(data)
-            self.p = -1 * (variance / mean - 1)
+            self.p = 1 - (variance/mean)
             n = mean/self.p
             self.n = round(n)
             self.p *= n/self.n

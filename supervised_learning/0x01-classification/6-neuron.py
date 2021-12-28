5e086673afe4a6cba5abd4e6427774774f80f8ba
Juand0145 <juand0145@gmail.com>
2021-12-28 17:38:14 -0500

    [Fixing] adding error exclusions to the task

--- supervised_learning/0x01-classification/6-neuron.py
+++ supervised_learning/0x01-classification/6-neuron.py
@@ -113,6 +113,14 @@ class Neuron:
         Y: is a numpy.ndarray with shape (1, m) that contains the correct
         labels for the input data
         """
+        if type(iterations) is not int:
+            raise TypeError("iterations must be an integer")
+        if iterations < 0:
+            raise ValueError("iterations must be a positive integer")
+        if type(alpha) is not float:
+            raise TypeError("alpha must be a float")
+        if alpha < 0:
+            raise ValueError("alpha must be positive")
 
         for i in range(iterations):
             self.forward_prop(X)

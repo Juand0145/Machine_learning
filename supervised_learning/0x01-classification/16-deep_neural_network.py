b952a94e3149a2636280fb4c1039dfafc67e05d2
Juan David Tuta Botero <juand0145@gmail.com>
2021-12-31 11:02:06 -0500

    [Fixing] task 16

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -17,29 +17,27 @@ class DeepNeuralNetwork:
         """
         if type(nx) != int:
             raise TypeError('nx must be an integer')
-        else:
-            if nx < 1:
-                raise ValueError('nx must be a positive integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
         if type(layers) != list:
             raise TypeError('layers must be a list of positive integers')
-        else:
-            if any(list(map(lambda x: x <= 0, layers))):
-                raise TypeError('layers must be a list of positive integers')
-            if len(layers) < 1:
-                raise TypeError('layers must be a list of positive integers')
+        if any(list(map(lambda x: x <= 0, layers))):
+            raise TypeError('layers must be a list of positive integers')
+        if len(layers) < 1:
+            raise TypeError('layers must be a list of positive integers')
 
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
 
         for i in range(self.L):
-            key = "W{}".format(i + 1)
+            key = f"W{i + 1}"
             if i == 0:
                 self.weights[key] = np.random.randn(layers[i],
                                                     nx)*np.sqrt(2/nx)
-                self.weights["b{}".format(i + 1)] = np.zeros((layers[i], 1))
+                self.weights[f"b{i + 1}"] = np.zeros((layers[i], 1))
             else:
                 self.weights[key] = (np.random.randn(layers[i],
                                                      layers[i - 1]) *
                                      np.sqrt(2/layers[i - 1]))
-                self.weights["b{}".format(i + 1)] = np.zeros((layers[i], 1))
+                self.weights[f"b{i + 1}"] = np.zeros((layers[i], 1))

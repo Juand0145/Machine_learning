05d1289037da68f0112fa9f7079c1943aa430b3e
Juan David Tuta Botero <juand0145@gmail.com>
2022-01-03 11:16:09 -0500

    [Adding] task 25

---
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -0,0 +1,30 @@
+#!usr/bin/env python3
+"""File that contains the function one_hot_decode"""
+import numpy as np
+from numpy.lib.function_base import append
+
+
+def one_hot_decode(one_hot):
+    """
+    Function that converts a one-hot matrix into a vector of labels
+    Args:
+    one_hot: is a one-hot encoded numpy.ndarray with shape (classes, m)
+        classes is the maximum number of classes
+        m is the number of examples
+    Returns: a numpy.ndarray with shape (m, ) containing the numeric
+    labels for each example, or None on failure
+    """
+    one_hot = one_hot.T
+
+    decode = []
+    rows = one_hot.shape[0]
+    columns = one_hot.shape[1]
+
+    for i in range(rows):
+        for j in range(columns):
+            if one_hot[i][j] == 1:
+                decode.append(j)
+
+    decode = np.array(decode)
+
+    return decode

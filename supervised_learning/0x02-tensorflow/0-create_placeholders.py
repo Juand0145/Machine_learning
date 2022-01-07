6977b4fb653f3043b8f627da85252818adf9bbb6
Juan David Tuta Botero <77861240+Juand0145@users.noreply.github.com>
2022-01-07 16:46:06 -0500

    [Fixing] task 0

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -17,4 +17,5 @@ def create_placeholders(nx, classes):
     """
     x = tf.placeholder(tf.float32, shape=[None, nx], name='x')
     y = tf.placeholder(tf.float32, shape=[None, classes], name='y')
+
     return x, y

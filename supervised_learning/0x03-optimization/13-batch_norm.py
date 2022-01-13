[Fixing] doing the correct normalization

supervised_learning/0x03-optimization/13-batch_norm.py
@@ -10,13 +10,17 @@ def batch_norm(Z, gamma, beta, epsilon):
     Z is a numpy.ndarray of shape (m, n) that should be normalized
         m is the number of data points
         n is the number of features in Z
-    gamma is a numpy.ndarray of shape (1, n) containing the scales
-    used for batch normalization
-    beta is a numpy.ndarray of shape (1, n) containing the offsets
-    used for batch normalization
+    gamma is a numpy.ndarray of shape (1, n) containing the scales used for batch normalization
+    beta is a numpy.ndarray of shape (1, n) containing the offsets used for batch normalization
     epsilon is a small number used to avoid division by zero
     Returns: the normalized Z matrix
     """
-    Z = (gamma + epsilon) * Z + beta
+    mean = Z.mean(axis=0)
+    variance = Z.var(axis=0)
+    Z_normalized = (Z - mean) / variance
 
-    return Z
+    gamma = pow(variance + epsilon, 0.5)
+    beta = mean
+    Z_batch_normalized = gamma * Z_normalized + beta
+
+    return Z_batch_normalized
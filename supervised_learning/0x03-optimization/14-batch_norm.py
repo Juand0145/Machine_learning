01187860acb88aa6906ffff794afd794f4312f3b
Juand0145 <juand0145@gmail.com>
2022-01-13 17:03:37 -0500

    [Adding] task 14

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -18,6 +18,7 @@ def create_batch_norm_layer(prev, n, activation):
     layer = tf.keras.layers.Dense(
         units=n, kernel_initializer=init, name='layer')
     epsilon = 1e-8
+
     base = layer(prev)
     gamma = tf.Variable(tf.constant(1.0, shape=[n]), trainable=True)
     beta = tf.Variable(tf.constant(0.0, shape=[n]), trainable=True)

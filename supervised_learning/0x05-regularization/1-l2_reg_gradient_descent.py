b0f9b2e425493f171de3688d146f580796faf02f
Juand0145 <juand0145@gmail.com>
2022-01-19 17:45:11 -0500

    [Fixing] changing the string format

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -20,26 +20,27 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """
     m = Y.shape[1]
     W_copy = weights.copy()
+    Layers = range(L + 1)[1:L + 1]
 
-    for i in reversed(range(L)):
-        A = cache["A" + str(i + 1)]
+    for i in reversed(Layers):
+        A = cache["A" + str(i)]
 
-        if i == L - 1:
-            dZ = cache["A" + str(i + 1)] - Y
-            dW = (np.matmul(cache["A" + str(i)], dZ.T) / m).T
+        if i == L:
+            dZ = cache["A" + str(i)] - Y
+            dW = (np.matmul(cache["A" + str(i - 1)], dZ.T) / m).T
 
-            dW_L2 = dW + (lambtha / m) * W_copy["W" + str(i + 1)]
+            dW_L2 = dW + (lambtha / m) * W_copy["W" + str(i)]
             db = np.sum(dZ, axis=1, keepdims=True) / m
 
         else:
-            dW2 = np.matmul(W_copy["W" + str(i + 2)].T, dZ2)
+            dW2 = np.matmul(W_copy["W" + str(i + 1)].T, dZ2)
             tanh = 1 - (A * A)
             dZ = dW2 * tanh
-            dW = np.matmul(dZ, cache["A" + str(i)].T) / m
+            dW = np.matmul(dZ, cache["A" + str(i - 1)].T) / m
 
-            dW_L2 = dW + (lambtha / m) * W_copy["W" + str(i + 1)]
+            dW_L2 = dW + (lambtha / m) * W_copy["W" + str(i)]
             db = np.sum(dZ, axis=1, keepdims=True) / m
 
-        weights["W" + str(i + 1)] = (W_copy["W" + str(i+1)] - (alpha * dW_L2))
-        weights["b" + str(i + 1)] = W_copy["b" + str(i + 1)] - (alpha * db)
+        weights["W" + str(i)] = (W_copy["W" + str(i)] - (alpha * dW_L2))
+        weights["b" + str(i)] = W_copy["b" + str(i)] - (alpha * db)
         dZ2 = dZ

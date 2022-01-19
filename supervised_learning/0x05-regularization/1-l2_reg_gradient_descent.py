9ba1b4518052c8a89727105889654bf02f65b7c6
Juand0145 <juand0145@gmail.com>
2022-01-18 22:00:59 -0500

    [Fixing] changing some dimentions

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -19,27 +19,27 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     L is the number of layers of the network
     """
     m = Y.shape[1]
-    W_copy = weights.copy()
+    Layers = range(L + 1)[1:L + 1]
 
-    for i in reversed(range(L)):
-        A = cache["A" + str(i + 1)]
+    for i in reversed(Layers):
+        A = cache[f"A{i}"]
 
-        if i == L - 1:
-            dZ = cache[f"A{i + 1}"] - Y
-            dW = (np.matmul(cache[f"A{i}"], dZ.T) / m).T
+        if i == L:
+            dZ = cache[f"A{i}"] - Y
+            dW = (np.matmul(cache[f"A{i - 1}"], dZ.T) / m).T
 
-            dW_L2 = dW + (lambtha / m) * W_copy[f"W{i + 1}"]
+            dW_L2 = dW + (lambtha / m) * weights[f"W{i}"]
             db = np.sum(dZ, axis=1, keepdims=True) / m
 
         else:
-            dW2 = np.matmul(W_copy[f"W{i + 2}"].T, dZ2)
+            dW2 = np.matmul(weights[f"W{i + 1}"].T, dZ2)
             tanh = 1 - (A * A)
             dZ = dW2 * tanh
-            dW = np.matmul(dZ, cache[f"A{i}"].T) / m
+            dW = np.matmul(dZ, cache[f"A{i - 1}"].T) / m
 
-            dW_L2 = dW + (lambtha / m) * W_copy[f"W{i + 1}"]
+            dW_L2 = dW + (lambtha / m) * weights[f"W{i}"]
             db = np.sum(dZ, axis=1, keepdims=True) / m
 
-        weights[f"W{i + 1}"] = (W_copy[f"W{i + 1}"]) - (alpha * dW_L2)
-        weights[f"b{i + 1}"] = W_copy[f"b{i + 1}"] - (alpha * db)
+        weights[f"W{i}"] = (weights[f"W{i}"]) - (alpha * dW_L2)
+        weights[f"b{i}"] = weights[f"b{i}"] - (alpha * db)
         dZ2 = dZ

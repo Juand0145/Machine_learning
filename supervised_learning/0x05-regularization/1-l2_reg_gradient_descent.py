ab4a87cb24083ec90e39181736f146fb072b812a
Juand0145 <juand0145@gmail.com>
2022-01-19 17:35:47 -0500

    [Testing] knwing if f instead of format works

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -22,19 +22,24 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     W_copy = weights.copy()
 
     for i in reversed(range(L)):
-        A = cache["A" + str(i + 1)]
+        A = cache[f"A{i + 1}"]
+    
         if i == L - 1:
             dZ = cache["A" + str(i + 1)] - Y
             dW = (np.matmul(cache["A" + str(i)], dZ.T) / m).T
+
             dW_L2 = dW + (lambtha / m) * W_copy["W" + str(i + 1)]
             db = np.sum(dZ, axis=1, keepdims=True) / m
+
         else:
             dW2 = np.matmul(W_copy["W" + str(i + 2)].T, dZ2)
             tanh = 1 - (A * A)
             dZ = dW2 * tanh
             dW = np.matmul(dZ, cache["A" + str(i)].T) / m
+
             dW_L2 = dW + (lambtha / m) * W_copy["W" + str(i + 1)]
             db = np.sum(dZ, axis=1, keepdims=True) / m
+
         weights["W" + str(i + 1)] = (W_copy["W" + str(i+1)] - (alpha * dW_L2))
         weights["b" + str(i + 1)] = W_copy["b" + str(i + 1)] - (alpha * db)
         dZ2 = dZ

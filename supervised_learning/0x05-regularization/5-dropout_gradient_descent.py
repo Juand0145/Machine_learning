c7f3037b2e3b515d4e87d1ae8c080b42b37448fa
Juand0145 <juand0145@gmail.com>
2022-01-20 11:14:58 -0500

    [Fixing] the pep 8 and using the current header

--- supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
+++ supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """File that contains the function dropout_gradient_descent"""
 import numpy as np
 
@@ -7,15 +7,18 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """
     Function that updates the weights of a neural network with Dropout
     regularization using gradient descent
-    Y is a one-hot numpy.ndarray of shape (classes, m) that contains the correct labels for the data
+    Y is a one-hot numpy.ndarray of shape (classes, m) that contains the
+    correct labels for the data
         classes is the number of classes
         m is the number of data points
     weights is a dictionary of the weights and biases of the neural network
-    cache is a dictionary of the outputs and dropout masks of each layer of the neural network
+    cache is a dictionary of the outputs and dropout masks of each layer of
+    the neural network
     alpha is the learning rate
     keep_prob is the probability that a node will be kept
     L is the number of layers of the network
-    All layers use thetanh activation function except the last, which uses the softmax activation function
+    All layers use thetanh activation function except the last, which uses the
+    softmax activation function
     The weights of the network should be updated in place
     """
     m = Y.shape[1]
@@ -26,13 +29,13 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
         A = cache["A" + str(i)]
 
         if i == L:
-            dZ = A -Y
+            dZ = A - Y
             dW = (np.matmul(cache["A" + str(i - 1)], dZ.T) / m).T
 
         else:
             dW2 = np.matmul(Weight_copy["W" + str(i + 1)].T, dZ2)
             dtanh = 1 - (pow(A, 2))
-            
+
             dZ = dW2 * dtanh
             dZ = dZ * cache["D" + str(i)]
             dZ = dZ/keep_prob
@@ -40,8 +43,8 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
             dW = np.matmul(dZ, cache["A" + str(i - 1)].T) / m
 
         db = np.sum(dZ, axis=1, keepdims=True) / m
-        
+
         weights["W" + str(i)] = (Weight_copy["W" + str(i)] - (alpha * dW))
         weights["b" + str(i)] = Weight_copy["b" + str(i)] - (alpha * db)
-        
+
         dZ2 = dZ

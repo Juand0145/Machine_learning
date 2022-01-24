01eb9db04ae8f8c733cf80aafa4b1cf9956794d4
Juand0145 <juand0145@gmail.com>
2022-01-24 13:03:12 -0500

    [Fixing] pep8

--- supervised_learning/0x06-keras/4-train.py
+++ supervised_learning/0x06-keras/4-train.py
@@ -10,13 +10,15 @@ def train_model(network, data, labels, batch_size, epochs,
     Args:
     network is the model to train
     data is a numpy.ndarray of shape (m, nx) containing the input data
-    labels is a one-hot numpy.ndarray of shape (m, classes) containing the labels of data
+    labels is a one-hot numpy.ndarray of shape (m, classes) containing the
+    labels of data
     batch_size is the size of the batch used for mini-batch gradient descent
     epochs is the number of passes through data for mini-batch gradient descent
-    verbose is a boolean that determines if output should be printed during training
+    verbose is a boolean that determines if output should be printed during
+    training
     shuffle is a boolean that determines whether to shuffle the batches
-    every epoch. Normally, it is a good idea to shuffle, but for reproducibility, we
-    have chosen to set the default to False.
+    every epoch. Normally, it is a good idea to shuffle, but for
+    reproducibility, we have chosen to set the default to False.
     Returns: the History object generated after training the model
     """
     train = network.fit(data,

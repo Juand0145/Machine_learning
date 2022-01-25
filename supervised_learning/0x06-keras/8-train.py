d24450f5ed853facf1da8c95a57e506f4811f4a1
Juand0145 <juand0145@gmail.com>
2022-01-25 08:49:15 -0500

    [Fixing] save best DOSN'T need the validation data

--- supervised_learning/0x06-keras/8-train.py
+++ supervised_learning/0x06-keras/8-train.py
@@ -18,25 +18,6 @@ def train_model(network, data, labels, batch_size, epochs,
     batch_size is the size of the batch used for mini-batch gradient descent
     epochs is the number of passes through data for mini-batch gradient descent
     validation_data is the data to validate the model with, if not None
-    early_stopping is a boolean that indicates whether early stopping
-    should be used:
-        early stopping should only be performed if validation_data exists
-        early stopping should be based on validation loss
-    patience is the patience used for early stopping
-    verbose is a boolean that determines if output should be printed
-    during training
-    shuffle is a boolean that determines whether to shuffle the batches
-    every epoch. Normally, it is a good idea to shuffle, but for
-    reproducibility, we have chosen to set the default to False.
-    learning_rate_decay is a boolean that indicates whether learning rate
-    decay should be used
-    learning rate decay should only be performed if validation_data exists
-    the decay should be performed using inverse time decay
-    the learning rate should decay in a stepwise fashion after each epoch
-    each time the learning rate updates, Keras should print a message
-    alpha is the initial learning rate
-    decay_rate is the decay rate
-    Returns: the History object generated after training the model
     """
     def learning_rate_decay(epoch):
         """Function tha uses the learning rate"""
@@ -55,9 +36,9 @@ def train_model(network, data, labels, batch_size, epochs,
                                                       verbose=1)
             callbacks.append(decay)
 
-        if save_best:
-            save = K.callbacks.ModelCheckpoint(filepath, save_best_only=True)
-            callbacks.append(save)
+    if save_best:
+        save = K.callbacks.ModelCheckpoint(filepath, save_best_only=True)
+        callbacks.append(save)
 
     train = network.fit(x=data, y=labels, batch_size=batch_size,
                         epochs=epochs, validation_data=validation_data,

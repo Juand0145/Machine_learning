f56212076c3a31da0f28ef86c2f24090446aa231
Juand0145 <juand0145@gmail.com>
2022-01-27 10:55:39 -0500

    [Fixing] pep8

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -3,15 +3,17 @@
 import numpy as np
 
 
-def convolve_grayscale_same(images, kernel): 
+def convolve_grayscale_same(images, kernel):
     """
     Function that performs a valid convolution on grayscale images:
     Args:
-    images is a numpy.ndarray with shape (m, h, w) containing multiple grayscale images
+    images is a numpy.ndarray with shape (m, h, w) containing multiple
+    grayscale images
         m is the number of images
         h is the height in pixels of the images
         w is the width in pixels of the images
-    kernel is a numpy.ndarray with shape (kh, kw) containing the kernel for the convolution
+    kernel is a numpy.ndarray with shape (kh, kw) containing the kernel for
+    the convolution
         kh is the height of the kernel
         kw is the width of the kernel
     Returns: a numpy.ndarray containing the convolved images
@@ -27,13 +29,13 @@ def convolve_grayscale_same(images, kernel):
     conv_h = h - kh + 1
     conv_w = w - kw + 1
 
-
     convolved_image = np.zeros((m, h, w))
 
     image = np.arange(m)
 
     for i in range(h)[1:conv_h]:
-        for j in range(w)[1:conv_w]:            
-            convolved_image[image, i, j] = np.sum(images[image, i:kh+i, j:kw+j] * kernel, axis=(1, 2))
+        for j in range(w)[1:conv_w]:
+            convolved_image[image, i, j] = np.sum(
+                images[image, i:kh+i, j:kw+j] * kernel, axis=(1, 2))
 
     return convolved_image

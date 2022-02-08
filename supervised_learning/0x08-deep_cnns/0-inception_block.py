450c759937575240e3356d7bd4f7be3461396440
Juand0145 <juand0145@gmail.com>
2022-02-07 20:27:32 -0500

    [Fixing] pycodestyle

--- supervised_learning/0x08-deep_cnns/0-inception_block.py
+++ supervised_learning/0x08-deep_cnns/0-inception_block.py
@@ -30,41 +30,41 @@ def inception_block(A_prev, filters):
 
     conv1 = K.layers.Conv2D(filters=F1,
                             kernel_size=1,
-                             padding='same',
-                               activation=function,
-                               kernel_initializer=initializer)(A_prev)
+                            padding='same',
+                            activation=function,
+                            kernel_initializer=initializer)(A_prev)
 
     conv2_P = K.layers.Conv2D(filters=F3R,
-                             kernel_size=1,
+                              kernel_size=1,
                               padding='same',
-                                activation=function,
-                                kernel_initializer=initializer)(A_prev)
+                              activation=function,
+                              kernel_initializer=initializer)(A_prev)
 
     conv2 = K.layers.Conv2D(filters=F3,
-                             kernel_size=3,
-                             padding='same',
-                               activation=function,
-                               kernel_initializer=initializer)(conv2_P)
+                            kernel_size=3,
+                            padding='same',
+                            activation=function,
+                            kernel_initializer=initializer)(conv2_P)
 
     conv3_p = K.layers.Conv2D(filters=F5R,
-                             kernel_size=1,
-                             padding='same',
-                                activation=function,
-                                kernel_initializer=initializer)(A_prev)
+                              kernel_size=1,
+                              padding='same',
+                              activation=function,
+                              kernel_initializer=initializer)(A_prev)
 
     conv3 = K.layers.Conv2D(filters=F5,
-                             kernel_size=5,
-                             padding='same',
-                               activation=function,
-                               kernel_initializer=initializer)(conv3_p)
+                            kernel_size=5,
+                            padding='same',
+                            activation=function,
+                            kernel_initializer=initializer)(conv3_p)
 
     layer_pool = K.layers.MaxPooling2D(pool_size=[3, 3],
-                                         strides=(1, 1),
+                                       strides=(1, 1),
                                        padding='same')(A_prev)
 
     layer_poolP = K.layers.Conv2D(filters=FPP,
-                                     kernel_size=1,
-                                     padding='same',
+                                  kernel_size=1,
+                                  padding='same',
                                   activation=function,
                                   kernel_initializer=initializer)(layer_pool)
 

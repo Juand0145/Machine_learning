ef2bf79f69a9275e6e22b5992b0aa9a9f745f594
Juand0145 <juand0145@gmail.com>
2022-02-09 11:49:11 -0500

    [Fixing] deleting the unuse exportation

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """File that contain the function inception_network"""
 import tensorflow.keras as K
-inception_block = __import__('0-inception_block').inception_block
 
 
 def inception_network():

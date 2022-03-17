49d843ce16dcdd5c3a6695731ffe47205bbacbf0
Juand0145 <juand0145@gmail.com>
2022-03-17 16:37:47 -0500

    [Fixing] quting the work import numpy as np

--- math/0x05-advanced_linear_algebra/5-definiteness.py
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -15,7 +15,6 @@ def definiteness(matrix):
     definite, positive semi-definite, negative semi-definite, negative
     definite of indefinite, respectively
     If matrix does not fit any of the above categories, return None
-    You may import numpy as np
     """
     if type(matrix) is not np.ndarray:
         raise TypeError("matrix must be a numpy.ndarray")

05ad24d86ae6b17e5a9f63729ac97e583e8afdd7
Juand0145 <juand0145@gmail.com>
2022-03-29 09:01:34 -0500

    [Adding] task 0 and README file

---
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""File that contains the function initialize"""
+import numpy as np
+from sklearn.cluster import KMeans
+
+
+def initialize(X, k):
+    """
+    Function that initializes cluster centroids for K-means
+    Args:
+    X is a numpy.ndarray of shape (n, d) containing the dataset that will
+    be used
+    for K-means clustering
+      n is the number of data points
+      d is the number of dimensions for each data point
+    k is a positive integer containing the number of clusters
+      The cluster centroids should be initialized with a multivariate uniform
+      distribution along each dimension in d:
+      The minimum values for the distribution should be the minimum values of
+      X along each dimension in d
+      The maximum values for the distribution should be the maximum values of
+      X along each dimension in d
+    You should use numpy.random.uniform exactly once
+    """
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None
+    if type(k) is not int or k <= 0:
+        return None
+
+    n, d = X.shape
+
+    minimun = np.min(X, axis=0)
+    maximun = np.max(X, axis=0)
+
+    centroids = np.random.uniform(minimun, maximun, size=(k, d))
+
+    return(centroids)
---
+++ unsupervised_learning/0x01-clustering/README.md
@@ -0,0 +1,36 @@
+# 0x01. Clustering
+
+In the following directory we will find a copilation of files wich porpuse is to understand the concepts of using Clustering and help us to answer the next questions.
+-   What is a multimodal distribution?
+-   What is a cluster?
+-   What is cluster analysis?
+-   What is “soft” vs “hard” clustering?
+-   What is K-means clustering?
+-   What are mixture models?
+-   What is a Gaussian Mixture Model (GMM)?
+-   What is the Expectation-Maximization (EM) algorithm?
+-   How to implement the EM algorithm for GMMs
+-   What is cluster variance?
+-   What is the mountain/elbow method?
+-   What is the Bayesian Information Criterion?
+-   How to determine the correct number of clusters
+-   What is Hierarchical clustering?
+-   What is Agglomerative clustering?
+-   What is Ward’s method?
+-   What is Cophenetic distance?
+-   What is  `scikit-learn`?
+-   What is  `scipy`?
+## Files
+ - 0-initialize.py
+ - 1-kmeans.py
+ - 2-variance.py
+ - 3-optimum.py
+ - 4-initialize.py
+ - 5-pdf.py
+ - 6-expectation.py
+ - 7-maximization.py
+ - 8-EM.py
+ - 9-BIC.py
+ - 10-kmeans.py
+ - 11-gmm.py
+ - 12-agglomerative.py

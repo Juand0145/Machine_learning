0f93bc2fe3468cc7effadb01c2661e7a76bcac5a
Juand0145 <juand0145@gmail.com>
2022-03-31 11:10:43 -0500

    [Fixing] modifyin the documentation and using k as parameter

--- unsupervised_learning/0x01-clustering/10-kmeans.py
+++ unsupervised_learning/0x01-clustering/10-kmeans.py
@@ -9,14 +9,13 @@ def kmeans(X, k):
     Args:
       X is a numpy.ndarray of shape (n, d) containing the dataset
       k is the number of clusters
-      The only import you are allowed to use is import sklearn.cluster
     Returns: C, clss
     C is a numpy.ndarray of shape (k, d) containing the centroid means for each
     cluster
     clss is a numpy.ndarray of shape (n,) containing the index of the cluster
     in C that each data point belongs to
     """
-    Kmean = sklearn.cluster.KMeans(n_clusters=5)
+    Kmean = sklearn.cluster.KMeans(n_clusters=k)
     Kmean.fit(X)
 
     C = Kmean.cluster_centers_

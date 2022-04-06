29834c95229b11e96b3d710fe77e2002eaa530d7
Juand0145 <juand0145@gmail.com>
2022-04-06 09:11:32 -0500

    [Fixing] task 2

--- unsupervised_learning/0x02-hmm/2-absorbing.py
+++ unsupervised_learning/0x02-hmm/2-absorbing.py
@@ -13,8 +13,20 @@ def absorbing(P):
             n is the number of states in the markov chain
         Returns: True if it is absorbing, or False on failure
     """
-    if np.all(P) != 1:
+    if len(P.shape) != 2:
+        return None
+    n1, n2 = P.shape
+    if (n1 != n2) or type(P) is not np.ndarray:
+        return None
+    D = np.diagonal(P)
+    if (D == 1).all():
         return True
-
-    else:
+    if not (D == 1).any():
         return False
+
+    for i in range(n1):
+        for j in range(n2):
+            if (i == j) and (i + 1 < len(P)):
+                if P[i + 1][j] == 0 and P[i][j + 1] == 0:
+                    return False
+    return True

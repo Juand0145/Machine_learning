c8d660bf9e2509ad92fc2443f6fbacafa13d0b67
Juand0145 <juand0145@gmail.com>
2022-04-13 17:45:14 -0500

    [Fixing] pycodestyle

--- unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
@@ -16,21 +16,21 @@ class BayesianOptimization:
         Class constructor initializer
             Args:
                 f is the black-box function to be optimized
-                X_init is a numpy.ndarray of shape (t, 1) representing the inputs
-                already sampled with the black-box function
-                Y_init is a numpy.ndarray of shape (t, 1) representing the outputs
-                of the black-box function for each input in X_init
+                X_init is a numpy.ndarray of shape (t, 1) representing the
+                inputs already sampled with the black-box function
+                Y_init is a numpy.ndarray of shape (t, 1) representing the
+                outputs of the black-box function for each input in X_init
                 t is the number of initial samples
                 bounds is a tuple of (min, max) representing the bounds of the
                 space in which to look for the optimal point
-                ac_samples is the number of samples that should be analyzed during
-                acquisition
+                ac_samples is the number of samples that should be analyzed
+                during acquisition
                 l is the length parameter for the kernel
                 sigma_f is the standard deviation given to the output of the
                 black-box function
                 xsi is the exploration-exploitation factor for acquisition
-                minimize is a bool determining whether optimization should be performed
-                for minimization (True) or maximization (False)
+                minimize is a bool determining whether optimization should be
+                performed for minimization (True) or maximization (False)
         """
         self.f = f
 

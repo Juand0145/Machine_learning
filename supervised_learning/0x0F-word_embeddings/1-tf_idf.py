82cde6b2efb65916b938159a5f9fd94eecc75dfc
Juand0145 <juand0145@gmail.com>
2022-05-09 16:04:56 -0500

    [Adding] task 1

---
+++ supervised_learning/0x0F-word_embeddings/1-tf_idf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""File that contains the function tf_idf"""
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+def tf_idf(sentences, vocab=None):
+    """
+    Function that creates a TF-IDF embedding
+    Args:
+      sentences is a list of sentences to analyze
+      vocab is a list of the vocabulary words to use for the analysis
+      If None, all words within sentences should be used
+  Returns: embeddings, features
+    embeddings is a numpy.ndarray of shape (s, f) containing the embeddings
+      s is the number of sentences in sentences
+      f is the number of features analyzed
+    features is a list of the features used for embeddings
+    """
+    if vocab is None:
+        vectorizer = TfidfVectorizer()
+        X = vectorizer.fit_transform(sentences)
+        vocab = vectorizer.get_feature_names()
+    else:
+        vectorizer = TfidfVectorizer(vocabulary=vocab)
+        X = vectorizer.fit_transform(sentences)
+    embedding = X.toarray()
+
+    return embedding, vocab

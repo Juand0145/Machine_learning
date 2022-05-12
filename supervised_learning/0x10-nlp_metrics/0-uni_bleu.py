abfdf0ca01d4b625e1f5c1f7a0cc5e8e6d7a5938
Juand0145 <juand0145@gmail.com>
2022-05-12 10:33:57 -0500

    [Adding] the README file and the task 0

---
+++ supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""File that contains the fucntion"""
+import numpy as np
+
+
+def uni_bleu(references, sentence):
+  """
+  Function that calculates the unigram BLEU score for a sentence
+  Args:
+    references is a list of reference translations
+    each reference translation is a list of the words in the translation
+    sentence is a list containing the model proposed sentence
+  Returns: the unigram BLEU score
+  """
+  sentence_length = len(sentence)
+  references_length = []
+  words = {}
+
+  for translation in references:
+      references_length.append(len(translation))
+      for word in translation:
+          if word in sentence and word not in words.keys():
+              words[word] = 1
+
+  total = sum(words.values())
+  index = np.argmin([abs(len(i) - sentence_length) for i in references])
+  best_match = len(references[index])
+
+  if sentence_length > best_match:
+      BLEU = 1
+  else:
+      BLEU = np.exp(1 - float(best_match) / float(sentence_length))
+  BLEU_score = BLEU * np.exp(np.log(total / sentence_length))
+
+  return BLEU_score
---
+++ supervised_learning/0x10-nlp_metrics/README.md
@@ -0,0 +1,11 @@
+# 0x10. Natural Language Processing - Evaluation Metrics
+In the following directory we will find a copilation of files wich porpuse is to understand how to use the Natural Language Processing - Evaluation Metrics and help us to answer the next questions.
+-   What are the applications of natural language processing?
+-   What is a BLEU score?
+-   What is a ROUGE score?
+-   What is perplexity?
+-   When should you use one evaluation metric over another?
+ ## Files
+ - 0-uni_bleu.py
+ - 1-ngram_bleu.py
+ - 2-cumulative_bleu.py

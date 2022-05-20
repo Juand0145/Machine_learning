b6cd9e9f305e6688d3c12ecd2bacfddc77a8115f
Juand0145 <juand0145@gmail.com>
2022-05-20 15:26:36 -0500

    [Fixing] pycodestyle

--- supervised_learning/0x11-attention/8-transformer_decoder_block.py
+++ supervised_learning/0x11-attention/8-transformer_decoder_block.py
@@ -6,6 +6,7 @@ MultiHeadAttention = __import__('6-multihead_attention').MultiHeadAttention
 
 class DecoderBlock(tf.keras.layers.Layer):
     """ class DecoderBlock """
+
     def __init__(self, dm, h, hidden, drop_rate=0.1):
         """
         Init method

6f9aaef0f6ef00b87c5bcee3275eec86b79ef577
Juand0145 <juand0145@gmail.com>
2022-07-25 10:06:36 -0500

    [Fixing] task 2

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -1,22 +1,31 @@
 #!/usr/bin/env python3
 """File that write a script that prints the location of a
 specific user"""
-import sys
 import requests
-import time
+from sys import argv
+from time import time
 
 
-if __name__ == '__main__':
-
-    url = sys.argv[1]
-    headers = {'Accept': 'application/vnd.github.v3+json'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        print(response.json()['location'])
-    if response.status_code == 404:
+if __name__ == "__main__":
+    if len(argv) < 2:
+        raise TypeError(
+            "Input must have the full API URL passed in as an argument: {}{}".
+            format('ex. "./2-user_location.py',
+                   'https://api.github.com/users/holbertonschool"'))
+    try:
+        url = argv[1]
+        results = requests.get(url)
+        if results.status_code == 403:
+            reset = results.headers.get('X-Ratelimit-Reset')
+            waitTime = int(reset) - time()
+            minutes = round(waitTime / 60)
+            print('Reset in {} min'.format(minutes))
+        else:
+            results = results.json()
+            location = results.get('location')
+            if location:
+                print(location)
+            else:
+                print('Not found')
+    except Exception as err:
         print('Not found')
-    if response.status_code == 403:
-        limit = int(response.headers['X-Ratelimit-Reset'])
-        start = int(time.time())
-        elapsed = int((limit - start) / 60)
-        print('Reset in {} min'.format(int(elapsed)))
